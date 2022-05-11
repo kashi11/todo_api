e2862cc3c58f441648c1cec756cf22689a55c01a
Kashif <kashif.ali@carbonteq.com>
2022-05-11 19:13:48 +0500

    Refactor

--- Bin/www.ts
+++ Bin/www.ts
@@ -1,6 +1,14 @@
 import Config from "../App/InfraStructure/Config";
 import app from "../HTTP/Bootstrap/App";
 import logger from "../App/InfraStructure/Logger/Logger";
-app.listen(Config.Server.APP_PORT, ()=>{
-  logger.debug(`${Config.Server.APP_NAME} is running on port ${Config.Server.APP_PORT}`);
-})
+import { Command } from "commander";
+const program = new Command();
+program.option("-p, --port <type>", "add the specified type of cheese", "3000");
+const options = program.opts();
+console.log(options);
+
+app.listen(options.port || Config.Server.APP_PORT, () => {
+  logger.debug(
+    `${Config.Server.APP_NAME} is running on port ${Config.Server.APP_PORT}`
+  );
+});
